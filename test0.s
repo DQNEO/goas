609d19e3a890d3b81c55f403f752a80fe07b676b
DQNEO <dqneoo@gmail.com>
2021-08-23 14:28:20 +0900

    Refactor test input

--- test0.s
+++ test0.s
@@ -2,15 +2,15 @@
 var1:
   .quad 0x10
 var2:
-  .quad var1
+  .quad 0x20
+  .quad 0x30
 var3:
-  .quad 0
-  .quad 0
-  .quad 0
-var4:
-  .quad var5
-var5:
   .quad var4
+var4:
+  .quad 0x40
+  .quad 0
+var5:
+  .quad var2
 
 .text
 .global _start
@@ -21,12 +21,12 @@ _start:
   ret
 
 myfunc1:
-#  movq (%rax), %rax
   movq var1(%rip), %rcx
+  movq $0x1a, %rcx
   ret
 myfunc2:
   movq $0x3c, %rax # sys_exit
-  movq $0x2a, %rdi # 42
+  movq $0x2a, %rdi # status code
   syscall
   ret
 myfunc3:
